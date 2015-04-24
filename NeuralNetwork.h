7370fea5aebc8563c2194c588b8489bde68a94a5
Adrian Bogatu <bogatu.adrian@gmail.com>
2015-04-24 13:59:12 +0300

    Create NeuralNetwork class with demo implementation

---
+++ NeuralNetwork.h
@@ -0,0 +1,24 @@
+#ifndef NEURAL_NET_H
+#define NEURAL_NET_H
+#include <vector>
+
+#include "utils.h"
+
+class NeuralNetwork {
+
+private:
+	std::vector<range> in_domain;
+	std::vector<range> out_domain;
+public:
+	NeuralNetwork(const std::vector<range> &in, const std::vector<range> &out) : in_domain(in), out_domain(out) {}
+
+	/* TODO: compute and return output */
+	virtual std::vector<int> input(const std::vector<int> &input) {
+		std::vector<int> res(input.size());
+		return res;
+	}
+	/* TODO: update network w/ given output */
+	virtual void output(const std::vector<int> &output) {
+	}
+};
+#endif
---
+++ utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+typedef std::pair<int, int> range;
+typedef std::function<std::vector<int>(const std::vector<int>)> function;
+
+#endif
+
